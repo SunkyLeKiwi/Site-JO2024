972367868f484c0268a1e0d21b323559e3d3a362
Chloé Aureau <chloe@ratigan.net>
2024-11-24 00:33:15 +0100

    Ajout site JO2024

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$db=mysqli_connect("dwarves.iut-fbleau.fr","aureau","UnTresBonMDPJO2024","aureau");
+$db=mysqli_connect("dwarves.iut-fbleau.fr","aureau","mdp","aureau");
 if (!$db){
 	echo "Vous n'êtes pas connecté : échec";
 }
